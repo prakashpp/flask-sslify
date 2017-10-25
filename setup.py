f6594cffa5ab464368b5135486a00b78d1d5cb32
Prakash Pandey <prakash.pandey@fulfil.io>
2017-10-25 17:32:53 +0530

    Bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 
 setup(
     name='Flask-SSLify',
-    version='0.1.6',
+    version='0.1.7',
     url='https://github.com/kennethreitz/flask-sslify',
     license='BSD',
     author='Kenneth Reitz',
